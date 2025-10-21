2c23a2519393ef751f56fba02cdfe07df47d1d92
victordsdsilva@gmail.com <victordsdsilva@gmail.com>
2025-10-20 21:49:30 -0300

    novos programas

--- dicionário/desafio_1.py
+++ dicionário/desafio_1.py
@@ -1,3 +1,19 @@
-frase = input("informe uma frase --> ")
+palavras = {"ola","mundo","python","programa","comite","bola" }
+qtd_letras = {}
+
+for palavra in palavras:
+
+    if f'qtd_letras - {len(palavra)}'not in qtd_letras.keys():
+        chave = f'qtd_palavras - {len(palavra)}'
+        valor = 1
+
+        qtd_letras.update({chave: valor})
+
+    else:
+        qtd_letras [f'qtd_palavra - {len(palavra)}'] += 1
+
+
+for chave, valor in qtd_letras.items():
+    print(f'{chave} | {valor} ')         
 
 
--- dicionário/desafio_2.py
+++ dicionário/desafio_2.py
@@ -1,8 +1,43 @@
-alunos = {
+alunos = {}
+opçao = -1
 
-"001": {"nome": "victor", "nota": 9},
-"002": {"nome": "joao", "nota": 7},
-"003": {"nome": "lucas", "nota": 6}
+while (opçao != 0):
+    opçao = int(input('o que deseja fazer ??? \n' \
+    'opçao 1: cadastrar aluno \n'
+    'opçao 2: exibir média \n' 
+    'opçao 3: exibir aluno com maior nota \n' 
+    'opçao 0: sair \n\n' 
+    'opcao ---> '))
 
-}
 
+    if opçao == 1:
+        key_aluno = len(alunos) +1
+        values_alunos = {}
+
+        nome_aluno = input('informe o nome do aluno --> ')
+        idade_aluno = int(input('informe a idade do aluno --> '))
+        nota_aluno = float(input('informe a nota do aluno --> '))
+        alunos.update({key_aluno: values_alunos})
+        print()
+
+    elif opçao == 2:
+        soma_notas = 0
+
+        for aluno in alunos.values():
+            soma_notas += alunos['nota']
+
+        print(f'a média das notas é {soma_notas / len(alunos)}')
+
+    elif opçao == 3:
+        maior = 0
+        id = 0
+
+    for id, aluno in alunos.items():
+        if aluno['nota'] > maior:
+            maior = aluno['nota']
+            id_alunos = id
+
+    print(f'o aluno com a maior nota é,{alunos[id_alunos]['nome']} | nota {maior}')  
+
+else:
+    print('\n opçao invalida \n')      
